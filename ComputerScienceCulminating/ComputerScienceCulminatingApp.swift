a8ee267b2b4f7ee3233060e6c96976d91bf6698b
Elaine Fang <efang@lcs.on.ca>
2025-04-15 17:32:43 -0400

    Initial Commit

---
+++ ComputerScienceCulminating/ComputerScienceCulminatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComputerScienceCulminatingApp.swift
+//  ComputerScienceCulminating
+//
+//  Created by Fang Elaine on 2025/4/15.
+//
+
+import SwiftUI
+
+@main
+struct ComputerScienceCulminatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComputerScienceCulminating/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ComputerScienceCulminating
+//
+//  Created by Fang Elaine on 2025/4/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
